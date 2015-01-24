f3909ae48908c6a4a93f1661c8a5d68a4bcdebdb
Daniel Szulc <szulc.d@gmail.com>
2015-01-24 16:51:04 +0100

    Skeleton of file with drabinka

---
+++ tournament_draw.R
@@ -0,0 +1,19 @@
+##ogolna funkcja zarzadzajaca przebiegiem turnieju
+tournament_general<-function()
+{
+        #
+}
+#funkcja przeprowadzajaca kwalifikacje do turnieju
+#zwraca liste rozstawionych i nierozstawionych ale zakwalifikowanych
+qualify <- function(lista)
+{
+        #funkcja korzysta z getranking() zdefiniowanej w pliku tensim.R
+}
+
+#funkcja tworzaca szkielet drabinki turniejowej
+#w zaleznosci od liczby graczy
+##tworzy globalny obiekt tr_draw
+make_draw <- function()
+{
+        #
+}
